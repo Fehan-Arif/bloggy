59c31916e6a22cbfbc49e15ce939bdac38f701ba
Fehan Arif <fehanarif@gmail.com>
2024-04-14 12:55:31 +1000

    Add view posts

--- index.js
+++ index.js
@@ -8,24 +8,46 @@ import express from "express";
 //  Constants
 //=====================
 const app = express(),
-  port = 3000;
-// __dirname = dirname(fileURLToPath(import.meta.url));
+  port = 3000,
+  // __dirname = dirname(fileURLToPath(import.meta.url));
+  posts = [
+  { category: 'Food', items: [] },
+  { category: 'Technology', items: [] },
+  { category: 'Travel', items: [] }
+  ];
 //=====================
 //  Middleware
 //=====================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 //=====================
+//  Functions
+//=====================
+function Post (title, content) {
+  this.title = title;
+  this.content = content;
+};
+// Constructor function to create an item
+// function createItem(title, content) {
+//   return { title, content };
+// }
+//=====================
 //  Routes
 //=====================
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-app.get("/posts", (req, res) => {
+app.get("/food", (req, res) => {
+  let post1 = new Post("The first Post", "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."); 
+  posts[0].items.push(post1);
+  
+  res.render("posts.ejs", { allPosts: posts});
+});
+app.get("/technology", (req, res) => {
   res.render("posts.ejs");
 });
-app.get("/post", (req, res) => {
-  res.render("post.ejs");
+app.get("/travel", (req, res) => {
+  res.render("posts.ejs");
 });
 app.get("/compose", (req, res) => {
   res.render("compose.ejs");
