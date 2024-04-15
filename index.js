6545b8570ad1021da71b6c5f2ad8da9ee63bc333
Fehan Arif <fehanarif@gmail.com>
2024-04-15 11:57:56 +1000

    Create post route for composing blogs. Add redirects to blog category pages.

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 app.get("/food", (req, res) => {
-  let post1 = new Post("The first Post", "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
-  posts[0].items.push(post1);
+  // let post1 = new Post("The first Post", "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
+  // posts[0].items.push(post1);
   let routePage = "Food";
   res.render("posts.ejs", { allPosts: posts, routePage: routePage });
 });
 app.get("/technology", (req, res) => {
-  let post1 = new Post('Latest Gadgets', 'Review of the new smartphone.');
-  posts[1].items.push(post1);
+  // let post1 = new Post('Latest Gadgets', 'Review of the new smartphone.');
+  // posts[1].items.push(post1);
   let routePage = "Technology";
   res.render("posts.ejs", { allPosts: posts, routePage: routePage });
 });
@@ -55,6 +55,19 @@ app.get("/travel", (req, res) => {
 });
 app.get("/compose", (req, res) => {
   res.render("compose.ejs");
+});
+app.post("/compose", (req, res) => {
+  // console.log(req.body);
+  let blogCategory = req.body.category;
+  for (let index = 0; index < posts.length; index++) {
+    let element = posts[index];
+    if (element.category === blogCategory) {
+      let newPost = new Post(req.body['blog-title'], req.body['blog-content']);
+      element.items.push(newPost);
+    }
+    // console.log(element.items);
+  }
+  res.redirect("/" + blogCategory.toLowerCase());
 });
 //=====================
 //  Listener
