84aadcd85187df2a69f990675f72e78ab52b3eb0
Fehan Arif <fehanarif@gmail.com>
2024-04-20 18:57:30 +1000

    Add Delete method. Update Post method

--- index.js
+++ index.js
@@ -3,62 +3,76 @@
 //=====================
 import express from "express";
 import methodOverride from "method-override";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
 //=====================
 //  Constants
 //=====================
 const app = express(),
   port = 3000,
-  // __dirname = dirname(fileURLToPath(import.meta.url));
   posts = [
-    { category: 'Food', items: [] },
-    { category: 'Technology', items: [] },
-    { category: 'Travel', items: [] }
+    {
+      category: 'food',
+      items: []
+    },
+    {
+      category: 'technology',
+      items: []
+    },
+    {
+      category: 'travel',
+      items: []
+    },
   ];
 //=====================
 //  Middleware
 //=====================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride(function(req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 //=====================
 //  Functions
 //=====================
 function Post(title, content) {
+  // Initialize a static counter for unique IDs
+  if (typeof Post.nextId === 'undefined') {
+    Post.nextId = 1;
+  }
+  // Assign the next available ID and increment the counter
+  this.id = Post.nextId++;
+  // Post Content
   this.title = title;
   this.content = content;
 };
-// Constructor function to create an item
-// function createItem(title, content) {
-//   return { title, content };
-// }
+//=====================
+//  Seed
+//=====================
+let post1 = new Post("The first Post", "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
+posts[0].items.push(post1);
+let post2 = new Post('Latest Gadgets', 'Review of the new smartphone.');
+posts[1].items.push(post2);
 //=====================
 //  Routes
 //=====================
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-app.get("/food", (req, res) => {
-  let post1 = new Post("The first Post", "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
-  posts[0].items.push(post1);
-  let routePage = "Food";
-  res.render("posts.ejs", { allPosts: posts, routePage: routePage });
+
+app.get("/posts/:postType", (req, res) => {
+  let postType = req.params.postType;
+  res.render("posts.ejs", { allPosts: posts, postType: postType });
 });
-app.get("/technology", (req, res) => {
-  // let post1 = new Post('Latest Gadgets', 'Review of the new smartphone.');
-  // posts[1].items.push(post1);
-  let routePage = "Technology";
-  res.render("posts.ejs", { allPosts: posts, routePage: routePage });
-});
-app.get("/travel", (req, res) => {
-  let routePage = "Travel";
-  res.render("posts.ejs", { allPosts: posts, routePage: routePage });
-});
+
 app.get("/compose", (req, res) => {
   res.render("compose.ejs");
 });
 app.post("/compose", (req, res) => {
-  let blogCategory = req.body.category;
+  let blogCategory = req.body.category.toLowerCase();
   for (let index = 0; index < posts.length; index++) {
     let element = posts[index];
     if (element.category === blogCategory) {
@@ -66,8 +80,9 @@ app.post("/compose", (req, res) => {
       element.items.push(newPost);
     }
   }
-  res.redirect("/" + blogCategory.toLowerCase());
+  res.redirect("/posts/" + blogCategory.toLowerCase());
 });
+
 app.get("/post/:postName", (req, res) => {
   let postName = req.params.postName;
   posts.forEach(post => {
@@ -79,6 +94,25 @@ app.get("/post/:postName", (req, res) => {
     })
   });
 });
+
+app.delete("/posts/:postType/delete", (req, res) => {
+  let id = parseInt(req.body.id);
+  let postType = req.body.postType;
+  if (postType === 'food') {
+    var i = 0;
+  } else if (postType === 'technology') {
+    var i = 1;
+  } else {
+    var i = 2;
+  };
+  // Find the index of the item with id 
+  let indexToDelete = posts[i].items.findIndex(item => item.id === id);
+  // Remove the item from the array
+  if (indexToDelete !== -1) {
+    posts[i].items.splice(indexToDelete, 1);
+  }
+  res.redirect('/posts/' + postType);
+})
 //=====================
 //  Listener
 //=====================
